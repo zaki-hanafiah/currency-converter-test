6b5e9d9af1e49adebfc4994950bd0b3b0baa5f56
zakhov <zakhov.vladimir@gmail.com>
2021-04-03 02:12:38 +0800

    fix: remove unused material modules

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -3,39 +3,29 @@ import { NgModule } from '@angular/core';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { CdkTreeModule } from '@angular/cdk/tree';
 import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRippleModule } from '@angular/material/core';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTreeModule } from '@angular/material/tree';
-import { MatTooltipModule } from '@angular/material/tooltip';
 
 const materialModules = [
 	CdkTreeModule,
 	MatButtonModule,
 	MatCardModule,
-	MatCheckboxModule,
-	MatDividerModule,
 	MatInputModule,
 	MatMenuModule,
-	MatProgressSpinnerModule,
 	MatRippleModule,
 	MatSelectModule,
 	MatSnackBarModule,
 	MatToolbarModule,
 	MatFormFieldModule,
-	MatButtonToggleModule,
 	MatTreeModule,
 	OverlayModule,
-	MatTooltipModule,
 ];
 
 @NgModule({
